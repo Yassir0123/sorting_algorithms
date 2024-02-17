6744ac3f9a2ae2edcf2b73a854dee7681030b886
Mzak05 <yassworld864@gmail.com>
2024-02-17 04:00:12 -0800

    set

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,35 +1,29 @@
 #include "sort.h"
 /**
-* selection_sort - Sorts an array using the selection sort algorithm
-* @array: The array to be sorted
-* @size: Number of elements in the array
+* selection_sort - Sortsorithm
+* @array: The array t
+* @size: lements in the array
 */
 void selection_sort(int *array, size_t size)
 {
-if (array == NULL)
+size_t i = 0, j, m, t;
+if (!array || size < 2)
 return;
-size_t i = 0, c, l, t, s;
-while (i < size)
+while (i < size - 1)
 {
-c = i;
-l = i;
-while (c < size)
+m = i;
+for (j = i + 1; j < size; ++j)
 {
-if (array[c] < array[l])
-{
-l = c;
-s = 1;
+if (array[j] < array[m])
+m = j;
 }
-c++;
-}
-if (s == 1)
+if (m != i)
 {
-t = array[l];
-array[l] = array[i];
-array[i] = t;
+t = array[i];
+array[i] = array[m];
+array[m] = t;
 print_array(array, size);
-s = 0;
 }
-i++;
+++i;
 }
 }
