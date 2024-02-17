7fba9b4fc61e0a7924dd47f810bfe62381ef88db
Mzak05 <yassworld864@gmail.com>
2024-02-17 03:48:16 -0800

    set

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,33 +1,26 @@
 #include "sort.h"
 /**
-* insertion_sort_list - insertion sorting algorithm
-* @list: linked list to sort
+* insertion_sort_list - Sorts a doubly linked list of integers
+* @list: Pointer to the head of the doubly linked list
 */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *tmp;
-	int n;
-
-	if (!list)
-		return;
-	tmp = *list;
-	while (tmp)
-	{
-		while (tmp)
-		{
-			if (tmp->next)
-			{
-				if (tmp->n > tmp->next->n)
-				{
-					n = tmp->n;
-					*(int *)&tmp->n = tmp->next->n;
-					*(int *)&tmp->next->n = n;
-					tmp = *list;
-					print_list(*list);
-					break;
-				}
-			}
-			tmp = tmp->next;
-		}
-	}
+listint_t *current, *next_node;
+int temp;
+if (list == NULL || *list == NULL)
+return;
+current = (*list)->next;
+while (current != NULL)
+{
+next_node = current->next;
+while (current->prev != NULL && current->n < current->prev->n)
+{
+temp = current->n;
+*(int *)&current->n = current->prev->n;
+*(int *)&current->prev->n = temp;
+current = current->prev;
+print_list(*list);
+}
+current = next_node;
+}
 }
