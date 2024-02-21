3e93615000bd76705c1c489a8f6e6950f46da0bd
Ztote Achraf <achraf.ztote@gmail.com>
2024-02-20 17:26:12 -0800

    optask

--- 0-main.c
+++ 0-main.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-    int array[] = {29, 18, 19, 71, 13, 52, 96, 73, 86, 7};
+    int array[] = {19, 48, 99, 61, 13, 52, 96, 73, 86, 7};
     size_t n = sizeof(array) / sizeof(array[0]);
 
     print_array(array, n);
